d7f24a583ab3bbc24c91841f070003732fafbf1d
thizzy824 <agpastora7@gmail.com>
2022-09-06 15:06:11 -0700

    grabbed button and input elements with querySelector

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ console.log('YESSIRRREEE')
 
 /*----- constants -----*/
 
+// Grab elements need for game using querySelector
 
+// buttons needed
+let foodButton = document.querySelector('.food')
 
+let sleepButton = document.querySelector('.sleep')
 
+let playButton = document.querySelector('.play')
+// input elements need for DOM
+let hungerInput = document.querySelector('#hunger')
 
+let ageInput = document.querySelector('#age')
 
+let boredInput = document.querySelector('#bored')
 
+let sleepInput = document.querySelector('#snooze')
 
 /*----- app's state (variables) -----*/
 
@@ -27,6 +37,8 @@ console.log('YESSIRRREEE')
 
 /*----- event listeners -----*/
 
+// Add event listeners to buttons
+
 
 
 
@@ -35,3 +47,5 @@ console.log('YESSIRRREEE')
 
 
 /*----- functions -----*/
+
+// create functions that will change/interact DOM
